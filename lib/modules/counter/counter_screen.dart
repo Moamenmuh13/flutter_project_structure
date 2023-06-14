7e7b380c901654ed6dd035e334ab351546e84aff
Mu'men Muhammad <61587687+Moamenmuh13@users.noreply.github.com>
2023-06-14 20:49:22 +0300

    after using cubit

--- lib/modules/counter/counter_screen.dart
+++ lib/modules/counter/counter_screen.dart
@@ -1,62 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_project_structure/modules/counter/cubit/cubit.dart';
-import 'package:flutter_project_structure/modules/counter/cubit/states.dart';
 
-class CounterScreen extends StatelessWidget {
-  CounterScreen({super.key});
+class CounterScreen extends StatefulWidget {
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int counter = 0 ;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => CounterCubit(),
-      child: BlocConsumer<CounterCubit, CounterStates>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("Counter App"),
+    return Scaffold(
+      appBar: AppBar(
+        title:  const Text("Counter App"),
+      ),
+      body:Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(onPressed: (){
+              setState(() {
+                counter++;
+              });
+            } , color: Colors.green, height: 90.0,child: const Text("+" , style: TextStyle(fontSize: 50.0),),) ,
+            const SizedBox(width: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text("${counter}" , style: const TextStyle(fontSize: 50.0),),
             ),
-            body: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  MaterialButton(
-                    onPressed: () {
-                      CounterCubit.get(context).plus();
-                    },
-                    color: Colors.green,
-                    height: 90.0,
-                    child: const Text(
-                      "+",
-                      style: TextStyle(fontSize: 50.0),
-                    ),
-                  ),
-                  const SizedBox(width: 20.0),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      "${CounterCubit.get(context).counter}",
-                      style: const TextStyle(fontSize: 50.0),
-                    ),
-                  ),
-                  const SizedBox(width: 20.0),
-                  MaterialButton(
-                    onPressed: () {
-                      CounterCubit.get(context).minus();
-                    },
-                    color: Colors.red[500],
-                    height: 90.0,
-                    child: const Text(
-                      "-",
-                      style: TextStyle(fontSize: 50.0),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+            const SizedBox(width: 20.0),
+            MaterialButton(onPressed: (){
+              setState(() {
+                if(counter > 0){
+                  counter--;
+                }else{
+                  print("No u can't");
+                }
+              });
+            } , color: Colors.red[500], height: 90.0,child: const Text("-" , style: TextStyle(fontSize: 50.0),),) ,
+          ],
+        ),
       ),
     );
   }
